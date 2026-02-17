5ccb4f42042f8274dc62c6afbf998902152f3151
Anvar <akbaralievanvar28@gmail.com>
2026-02-17 10:06:11 +0100

    Week2 task

--- Week-2/src/Main.java
+++ Week-2/src/Main.java
@@ -1,9 +1,58 @@
-void main() {
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Task 1
+
+        System.out.print("Temperature: ");
+        int temp = scanner.nextInt();
+
+        if (temp < 0)
+            System.out.println("Below_freezing");
+        if (temp >= 0 && temp < 30)
+            System.out.println("Average");
+        if (temp >= 30)
+            System.out.println("Too hot");
+
+        // Task 2
+
+        int sec;
+        do {
+            System.out.print("Seconds_after_lightning: ");
+            sec = scanner.nextInt();
+        } while (sec < 0);
+
+        System.out.println("Distance: " + (sec * 300) + " meters");
+
+        // Task 3
+
+        int age;
+        do {
+            System.out.print("How old are you?(between 1-20): ");
+            age = scanner.nextInt();
+        } while (age < 1 || age > 20);
+
+        // Draw cake
+        for (int i = 0; i < age; i++) {
+            System.out.print("* ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < age; i++) {
+            System.out.print("| ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < age; i++) {
+            System.out.print("--");
+        }
+        System.out.println();
+
+        System.out.print("_____END_PROGRAM____");
 
 
+        scanner.close();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,46 @@
 import java.util.Scanner;
 
+// My moodle course is https://moodle.nye.hu/course/view.php?id=2887
+// Enrollment key is 20_Java_26
+
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Task 1
-
-        System.out.print("Temperature: ");
-        int temp = scanner.nextInt();
-
-        if (temp < 0)
-            System.out.println("Below_freezing");
-        if (temp >= 0 && temp < 30)
-            System.out.println("Average");
-        if (temp >= 30)
-            System.out.println("Too hot");
-
-        // Task 2
-
-        int sec;
-        do {
-            System.out.print("Seconds_after_lightning: ");
-            sec = scanner.nextInt();
-        } while (sec < 0);
-
-        System.out.println("Distance: " + (sec * 300) + " meters");
-
-        // Task 3
-
-        int age;
-        do {
-            System.out.print("How old are you?(between 1-20): ");
-            age = scanner.nextInt();
-        } while (age < 1 || age > 20);
-
-        // Draw cake
-        for (int i = 0; i < age; i++) {
-            System.out.print("* ");
+        //Literals:
+        int number = 42; // Decimal number
+        int number2 = 0x42; // Hexadecimal number
+        int number3 = 042; // Octal number
+        float real_number = 3.14f; // A real float number
+        double real_number2 = 3.14; // A real double number
+        char letter = '\u263A'; // Unicode character, not ASCII
+        boolean bool = true; // Boolean type, true or false
+        String sentence = "appletree"; // String is a class, not a primitive type
+        System.out.println("Number: " + number);
+        System.out.println("Number2: " + number2);
+        System.out.println("Number3: " + number3);
+        System.out.println("real_number: " + real_number);
+        System.out.println("real_number2: " + real_number2);
+        System.out.println("Letter: " + letter);
+        if (bool) {
+            System.out.println("bool is true");
+        } else
+        {
+            System.out.println("bool is false");
         }
-        System.out.println();
+        System.out.println(sentence.toUpperCase());
+        System.out.println(sentence.substring(5));
 
-        for (int i = 0; i < age; i++) {
-            System.out.print("| ");
-        }
-        System.out.println();
+        //Ask a number from the keyboard:
+        //Create a Scanner object
+        Scanner input = new Scanner(System.in);
+        //Use the Scanner object to send data to a variable:
+        System.out.print("Enter an integer number: ");
+        int number1 = Integer.parseInt(input.nextLine()); //Send a string, then convert to an integer
+        System.out.println("number1: " + number1);
+        System.out.println("Enter your name:");
+        //String name = input.next(); //Send until the first whitespace character
+        //input.nextLine(); //An empty input, to free the previous ENTER
+        String name = input.nextLine(); //Send until the first ENTER
+        System.out.println("your name: " + name);
 
-        for (int i = 0; i < age; i++) {
-            System.out.print("--");
-        }
-        System.out.println();
-
-        System.out.print("_____END_PROGRAM____");
-
-
-        scanner.close();
     }
 }
